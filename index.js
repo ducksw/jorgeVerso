26345589503aa88b841b0dcc1c95940c36fe1bcc
ducks <cansgamer04@gmail.com>
2024-03-08 23:56:07 -0500

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById('gen').setAttribute('onclick', 'generator()')
 document.getElementById('copy').setAttribute('onclick', 'copiar()')
 document.getElementById('btn-info').setAttribute('onclick', 'info()')
 
-const txt = "43pgpvwinkK4HCxJvEH7Fs2KMkH1VBCVqRjyCVLfCwTrGD1NzGSHkmuDUyFqCjBiCPLYePT1StfQn1uZhkPJczzzFXjYZ1U";
+const txt = "43pgpvwin8479kK4HCxJvEH7Fs2KMkH1VBCVqRjyCVLfCwTrGD1NzGSH59864kmuDUyFqCjBiCPLYePT1StfQn1u015384ZhkPJczzzFXjYZ1U";
 var cont = 0;
 let cn = 0
 
@@ -19,46 +19,6 @@ function ran() {
   return r
 }
 
-function call() {
-  let inp = document.getElementById('input').value
-  let result = document.getElementById('result').innerHTML 
-
-  const image = [
-    { name: "Jorgito tu Terror", src: "https://e.radio-grpp.io/normal/2022/03/25/074807_1237177.jpg" },
-    { name: "Ricardo el Inmortal", src: "https://larepublica.cronosmedia.glr.pe/original/2022/01/18/61e73bca2b61d2649f3d40cb.jpg" },
-    { name: "Gerardo", src: "https://imgmedia.larepublica.pe/640x640/larepublica/migration/images/SMDAWVNKANE3RBAJVZBHIIKU4U.webp" },
-    { name: "El Cojo", src: "https://larepublica.cronosmedia.glr.pe/migration/images/MW3DA4AKQFCCHMRNEFGO42S3LQ.jpg" },
-  ]
-
-  if (inp != result || inp == "") {
-    alert("GENERA UN CODIGO")
-  } else {
-    document.getElementById('result').innerHTML = "Cargando..."
-
-    let pic = document.getElementById('image')
-
-    setTimeout(function() {
-      cn++
-
-      if (cn >= 1) {
-        let randName = Math.floor(Math.random() * image.length)
-        let img = image[randName]
-
-        pic.style.backgroundImage = `url(${img.src})`
-        //pic.style.backgroundSize = "cover"
-        //pic.style.transition = "all 3s"
-        //pic.style.width = "300px"
-        //pic.style.border = "1px dotted black"
-
-        document.getElementById('result').innerHTML = "ERESSSS"
-        document.getElementById('result2').innerHTML = img.name
-        console.log(img)
-      }
-    }, 3000)
-  }
-  document.getElementById('input').value = ""
-}
-
 function generator() {
   var limit = setInterval(function() {
     cont++
@@ -71,9 +31,77 @@ function generator() {
     if (cont >= 1) {
       clearInterval(limit)
     }
-  }, 1000)
+  }, 2500)
 
   document.getElementById('result').innerHTML = "Pending..."
+}
+
+function call() {
+  let inp = document.getElementById('input').value
+  let result = document.getElementById('result').innerHTML 
+
+  // ### Personajes DEFAULT ###
+  const image = [
+    { 
+      name: "Jorgito tu Terror",
+      src: "https://e.radio-grpp.io/normal/2022/03/25/074807_1237177.jpg" 
+    },
+    {
+      name: "Ricardo el Inmortal",
+      src: "https://larepublica.cronosmedia.glr.pe/original/2022/01/18/61e73bca2b61d2649f3d40cb.jpg"
+    },
+    {
+      name: "Gerardo",
+      src: "https://imgmedia.larepublica.pe/640x640/larepublica/migration/images/SMDAWVNKANE3RBAJVZBHIIKU4U.webp"
+    },
+    { name: "El Cojo",
+      src: "https://larepublica.cronosmedia.glr.pe/migration/images/MW3DA4AKQFCCHMRNEFGO42S3LQ.jpg"
+    },
+    {
+      name: "Dayanita",
+      src: "https://e.rpp-noticias.io/xlarge/2023/04/27/022602_1420807.jpg"
+    },
+    {
+      name: "Chupetin Trujillo",
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQQHYufIQ03fYC0Y5e-Q9uLvFXcnGxayTW-9X6iPMTGw&s"
+    },
+    {
+      name: "Cristian Dominguez",
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr8pKW0mSbvroLzw4mlnGkcH98QjRKrsP4I3HYqtCHow&s"
+    },
+    {
+      name: "El carro de Cristian Dominguez",
+      src: "https://i0.wp.com/informateperu.pe/wp-content/uploads/2024/01/image-73.png?resize=1200%2C675&ssl=1",
+    }
+  ]
+
+  if (inp != result || inp == "") {
+    alert("GENERA UN CODIGO")
+  } else {
+    document.getElementById('result').innerHTML = "Cargando..."
+
+    let pic = document.getElementById('image')
+
+    setTimeout(function() {
+      cn++
+      if (cn >= 1) {
+        let randName = Math.floor(Math.random() * image.length)
+        let img = image[randName]
+
+        pic.style.backgroundImage = `url(${img.src})`
+        //pic.style.backgroundSize = "cover"
+        //pic.style.transition = "all 3s"
+        //pic.style.width = "300px,
+        //pic.style.border = "1px dotted black"
+
+        document.getElementById('result').innerHTML = "Eres"
+        document.getElementById('result2').innerHTML = img.name
+        document.getElementById('result2').innerHTML = img.color
+        console.log(img)
+      }
+    }, 3000)
+  }
+  document.getElementById('input').value = ""
 }
 
 function copiar() {
@@ -95,6 +123,5 @@ function info() {
     fr.style.display = "none";
     frcont--
   }
-
-  console.log("a");
 }
+
