440ca990f7b7f603f220a28188546233fc9586ac
duksito <cansgamer04@gmail.com>
2024-03-10 11:01:49 -0500

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.getElementById('btn').setAttribute('onclick', 'call()')
 document.getElementById('gen').setAttribute('onclick', 'generator()')
 document.getElementById('copy').setAttribute('onclick', 'copiar()')
 document.getElementById('btn-info').setAttribute('onclick', 'info()')
+document.getElementById('imageup').setAttribute('onclick', 'upLink()')
 
 const txt = "43pgpvwin8479kK4HCxJvEH7Fs2KMkH1VBCVqRjyCVLfCwTrGD1NzGSH59864kmuDUyFqCjBiCPLYePT1StfQn1u015384ZhkPJczzzFXjYZ1U";
 var cont = 0;
@@ -35,47 +36,46 @@ function generator() {
 
   document.getElementById('result').innerHTML = "Pending..."
 }
+// ### Personajes DEFAULT ###
+let image = [
+  { 
+    name: "Jorgito tu Terror",
+    src: "https://e.radio-grpp.io/normal/2022/03/25/074807_1237177.jpg" 
+  },
+  {
+    name: "Ricardo el Inmortal",
+    src: "https://larepublica.cronosmedia.glr.pe/original/2022/01/18/61e73bca2b61d2649f3d40cb.jpg"
+  },
+  {
+    name: "Gerardo",
+    src: "https://imgmedia.larepublica.pe/640x640/larepublica/migration/images/SMDAWVNKANE3RBAJVZBHIIKU4U.webp"
+  },
+  { name: "El Cojo",
+    src: "https://larepublica.cronosmedia.glr.pe/migration/images/MW3DA4AKQFCCHMRNEFGO42S3LQ.jpg"
+  },
+  {
+    name: "Dayanita",
+    src: "https://e.rpp-noticias.io/xlarge/2023/04/27/022602_1420807.jpg"
+  },
+  {
+    name: "Chupetin Trujillo",
+    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQQHYufIQ03fYC0Y5e-Q9uLvFXcnGxayTW-9X6iPMTGw&s"
+  },
+  {
+    name: "Cristian Dominguez",
+    src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr8pKW0mSbvroLzw4mlnGkcH98QjRKrsP4I3HYqtCHow&s"
+  },
+  {
+    name: "El carro de Cristian Dominguez",
+    src: "https://i0.wp.com/informateperu.pe/wp-content/uploads/2024/01/image-73.png?resize=1200%2C675&ssl=1",
+  },
+]
 
 function call() {
   let inp = document.getElementById('input').value
   let result = document.getElementById('result').innerHTML 
 
-  // ### Personajes DEFAULT ###
-  const image = [
-    { 
-      name: "Jorgito tu Terror",
-      src: "https://e.radio-grpp.io/normal/2022/03/25/074807_1237177.jpg" 
-    },
-    {
-      name: "Ricardo el Inmortal",
-      src: "https://larepublica.cronosmedia.glr.pe/original/2022/01/18/61e73bca2b61d2649f3d40cb.jpg"
-    },
-    {
-      name: "Gerardo",
-      src: "https://imgmedia.larepublica.pe/640x640/larepublica/migration/images/SMDAWVNKANE3RBAJVZBHIIKU4U.webp"
-    },
-    { name: "El Cojo",
-      src: "https://larepublica.cronosmedia.glr.pe/migration/images/MW3DA4AKQFCCHMRNEFGO42S3LQ.jpg"
-    },
-    {
-      name: "Dayanita",
-      src: "https://e.rpp-noticias.io/xlarge/2023/04/27/022602_1420807.jpg"
-    },
-    {
-      name: "Chupetin Trujillo",
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQQHYufIQ03fYC0Y5e-Q9uLvFXcnGxayTW-9X6iPMTGw&s"
-    },
-    {
-      name: "Cristian Dominguez",
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr8pKW0mSbvroLzw4mlnGkcH98QjRKrsP4I3HYqtCHow&s"
-    },
-    {
-      name: "El carro de Cristian Dominguez",
-      src: "https://i0.wp.com/informateperu.pe/wp-content/uploads/2024/01/image-73.png?resize=1200%2C675&ssl=1",
-    }
-  ]
-
-  if (inp != result || inp == "") {
+   if (inp != result || inp == "") {
     alert("GENERA UN CODIGO")
   } else {
     document.getElementById('result').innerHTML = "Cargando..."
@@ -103,16 +103,48 @@ function call() {
   document.getElementById('input').value = ""
 }
 
+// LLAMAR LA FUNCION AL DAR ENTER
+document.getElementById('input').addEventListener('keypress', function(event) {
+  if (event.keyCode === 13) {
+    call();
+  }
+});
+
 function copiar() {
   let result = document.getElementById('result').innerHTML 
   navigator.clipboard.writeText(result)
 }
 
+function upArchive() {
+  let archivo = document.getElementById("file").files[0];
+  let rd = new FileReader();
+
+  if (file) {
+    let nm = prompt("Coloca del personaje")
+    rd.readAsDataURL(archivo );
+    rd.onloadend = function () {
+      //document.getElementById("img").src = reader.result;
+      image.push({name: nm, src: rd.result})
+    }
+    console.log(image)
+  }
+}
+
+function upLink() {
+  let nm = prompt("Coloca del personaje")
+  let URL = prompt("Coloca el URL")
+
+  if (nm == ""|| URL == "") {
+    alert("Complete todos los campos")
+  } else {
+    image.push({name: nm, src: URL})
+  }
+  console.log(image)
+}
 
 var frcont = 0
 function info() {
   let fr = document.getElementById('frame')
-  let bd = document.getElementById('body')
 
   if (frcont < 1) {
     fr.style.display = "block";
@@ -123,4 +155,3 @@ function info() {
     frcont--
   }
 }
-
